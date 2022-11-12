d93560c41b751e6ef662e6b4f6be9f2c9dc52f1b
elfen95.ealc <elfen95.ealc@gmail.com>
2022-11-11 20:41:56 -0600

    manejo de estados

--- src/components/TodoSearch.tsx
+++ src/components/TodoSearch.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "../styles/TodoSearch.css";
 
 export const TodoSearch = () => {
+  const [searchValue, setSearchValue] = useState<string>("");
+
   const onSearchValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
 
   return (
-    <input
-      className="TodoSearch"
-      type="text"
-      placeholder="Sistema"
-      onChange={onSearchValueChange}
-    />
+    <>
+      <input
+        className="TodoSearch"
+        type="text"
+        placeholder="Sistema"
+        value={searchValue}
+        onChange={onSearchValueChange}
+      />
+      <span>{searchValue}</span>
+    </>
   );
 };
